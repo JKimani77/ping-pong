8119b30b92134cb742113256ebaa5301e55861e5
Jkimani77 <jkkimani77@gmail.com>
2018-05-04 14:57:09 +0300

    Adds user interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
+// Business Logic.
+function mypingfunction(myresult){
+  var pparray = [];
 
+  for (var i = 1; i <= myresult; i++) {
+    if ((i%3) == 0) {
+      pparray.push("ping");
+    }else if ((i%5) == 0 ){
+      pparray.push("pong");
+    }else if ((i%15) == 0) {
+      pparray.push("pingpong");
+    }else {
+      pparray.push(i);
+    }
+  }
+
+
+
+
+// User Interface.
+$(document).ready(function(){
+  $("form#pingform").submit(function(event){
+       // event.preventDefault();
+    $("#Results").empty();
+    var myresult = parseInt($("input#anumeral").val());
+     var pparray = mypingfunction(myresult);
+     pparray.forEach(function(myouttput){
+       $("#Results").append(myouttput + "</br>");
+       event.preventDefault();
+     });
+
+  });
+});
